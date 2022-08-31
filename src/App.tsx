f7430a19a6130a351b8288085a9138fbad1ee61e
joao <joao4729@gmail.com>
2022-08-30 22:01:03 -0300

    a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-   font-size:64px;
-   color:#8257e6;
-`;
-
+import { Dashboard } from "./Components/Dashboard";
+import { Header } from "./Components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
 
-       <Title>Hello word </Title>
-    </div>
+    </>
   );
 }
-        
-      
-
-
